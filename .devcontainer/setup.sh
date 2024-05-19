86aa3e696b1e4b0ce1be7906793fd45d40bac0c8
dskydiver <109866033+dskydiver@users.noreply.github.com>
2024-05-19 16:58:09 +0000

    update dev container setup

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -16,6 +16,7 @@ apt-get install -y \
 
 ## Install rustup and common components
 curl https://sh.rustup.rs -sSf | sh -s -- -y 
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 source $HOME/.cargo/env
 
 rustup install nightly
@@ -34,7 +35,11 @@ cp /root/.zshrc /home/$USERNAME
 sed -i -e "s/\/root\/.oh-my-zsh/\/home\/$USERNAME\/.oh-my-zsh/g" /home/$USERNAME/.zshrc
 chown -R $USER_UID:$USER_GID /home/$USERNAME/.oh-my-zsh /home/$USERNAME/.zshrc
 
+## install firacode
+sudo add-apt-repository universe
+sudo apt-get install fonts-firacode
+
 # install solana
-sh -c "$(curl -sSfL https://release.solana.com/v1.18.4/install)"
+sh -c "$(curl -sSfL https://release.solana.com/v1.18.9/install)"
 
 echo 'PATH=$PATH:/$PATH:/opt/solana-1.18.4/bin' >> ~/.bashrc
